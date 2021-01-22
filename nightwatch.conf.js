5c316aa7d4e680843e5a4d7c734f9570cac48822
Robert DeLuca <robertdeluca19@gmail.com>
2021-01-22 17:05:16 -0600

    fix CI

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,19 +1,20 @@
-const chromedriver = require('chromedriver')
-const seleniumServer = require('selenium-server')
-const percy = require('@percy/nightwatch')
+const chromedriver = require('chromedriver');
+const seleniumServer = require('selenium-server');
+const percy = require('@percy/nightwatch');
 
 module.exports = {
   src_folders: ['tests'],
   output_folder: false,
-  custom_commands_path: [ percy.path ],
+  custom_commands_path: [percy.path],
 
   selenium: {
     start_process: true,
     server_path: seleniumServer.path,
+    check_process_delay: 7000,
     log_path: false,
     cli_args: {
-      'webdriver.chrome.driver': chromedriver.path,
-    },
+      'webdriver.chrome.driver': chromedriver.path
+    }
   },
 
   test_settings: {
@@ -24,15 +25,15 @@ module.exports = {
         chromeOptions: {
           args: ['headless'],
           w3c: false
-        },
-      },
+        }
+      }
     },
 
     Chrome: {
       desiredCapabilities: {
         browserName: 'chrome',
-        javascriptEnabled: true,
-      },
-    },
-  },
-}
+        javascriptEnabled: true
+      }
+    }
+  }
+};
