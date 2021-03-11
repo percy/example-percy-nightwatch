ac0fbccc3ed68066192a2d83448eb686dfc28e73
Robert DeLuca <robertdeluca19@gmail.com>
2021-03-11 13:26:12 -0600

    :sparkles: Update example TodoMVC app & Percy SDK

--- tests/todomvc.test.js
+++ tests/todomvc.test.js
@@ -1,45 +1,42 @@
-const httpServer = require('http-server')
+const httpServer = require('http-server');
 
-const PORT = 8000
-const TEST_URL = `http://localhost:${PORT}`
-var server
+const PORT = 8000;
+const TEST_URL = `http://localhost:${PORT}`;
+let server;
 
 module.exports = {
-  before: function(browser) {
+  before: function (browser) {
     // Start a local server to serve the TodoMVC app.
-    server = httpServer.createServer()
-    server.listen(PORT)
+    server = httpServer.createServer({ root: `${__dirname}/../` });
+    server.listen(PORT);
   },
 
-  after: function(browser) {
+  after: function (browser) {
     // Shutdown the http server.
-    server.close()
+    server.close();
     // Close down the browser session.
-    browser.end()
+    browser.end();
   },
 
-  afterEach: function(browser) {
+  afterEach: function (browser) {
     // Clear local storage between test cases.
-    browser.execute('window.localStorage.clear()')
+    browser.execute('window.localStorage.clear()');
   },
 
-  'Loads the app': function(browser) {
-    browser
-      .url(TEST_URL)
-      .assert.visible('section.todoapp')
-      .percySnapshot()
+  'Loads the app': function (browser) {
+    browser.url(TEST_URL).assert.visible('section.todoapp').percySnapshot();
   },
 
-  'Accepts a new todo': function(browser) {
+  'Accepts a new todo': function (browser) {
     browser
       .url(TEST_URL)
       .sendKeys('.new-todo', 'New fancy todo')
       .sendKeys('.new-todo', browser.Keys.ENTER)
       .assert.containsText('.todo-list li:first-child label', 'New fancy todo')
-      .percySnapshot(browser.currentTest.name, { widths: [768, 992, 1200] })
+      .percySnapshot(browser.currentTest.name, { widths: [768, 992, 1200] });
   },
 
-  'Lets you check off a todo': function(browser) {
+  'Lets you check off a todo': function (browser) {
     browser
       .url(TEST_URL)
       .sendKeys('.new-todo', 'A thing to accomplish')
@@ -47,6 +44,6 @@ module.exports = {
       .assert.containsText('.todo-count', '1 item left')
       .click('input.toggle')
       .assert.containsText('.todo-count', '0 items left')
-      .percySnapshot('Checked-off todo')
-  },
-}
+      .percySnapshot('Checked-off todo');
+  }
+};
